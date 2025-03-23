2be37942e8c15e1d0296af5d5a5f59f9fa9bc022
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-03-23 13:47:00 -0600

    feat: add findAll method rendering in frontend
    - add html component for product rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NgForOf, NgIf} from '@angular/common';
+import {ProductComponent} from './products/components/product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, NgForOf],
+  imports: [RouterOutlet, NgIf, NgForOf, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
